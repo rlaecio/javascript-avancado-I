8c6d43f7b99a8b0d5c14f270ec78012cfe81863b
Gitlab.network <roque.souza@portodigital.pt>
2021-01-04 22:40:10 +0000

    imutabilidade

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,13 +1,19 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+
+        Object.freeze(this);
+    }
+
+    get volume() {
+        return this._quantidade * this.valor;
     }
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
     
     get quantidade() {
@@ -18,7 +24,4 @@ class Negociacao {
         return this._valor;
     }
 
-    get volume() {
-        return this._quantidade * this.valor;
-    }
 }
