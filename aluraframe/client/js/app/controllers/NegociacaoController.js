e64f7098518a598684141d03c43bdb8d0f712db7
Gitlab.network <roque.souza@portodigital.pt>
2021-01-05 12:58:48 +0000

    Update NegociacaoController.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,24 +9,19 @@ class NegociacaoController {
     
     adiciona () {
         event.preventDefault();
-        let data = new Date(...
-            this._inputData.value
+        let data = new Date(
+            ...this._inputData.value
             .split('-')
-            .map(function(item, indice) {
-                return item - indice % 2
-            })
-            
+            .map((item, indice) => item - indice % 2)  
         );
-        console.log(data);
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
 
-
-        // let negociacao = new Negociacao(
-        //     this._inputData.value.split('-'),
-        //     this._inputQuantidade.value,
-        //     this._inputValor.value
-        // );
-
-        // console.log(negociacao);
+        console.log(negociacao);
 
         // adicionar a negociaçao numa lista
     }
