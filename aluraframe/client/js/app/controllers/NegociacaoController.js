8e1a129c55ef646da4f0538f859e26205c0363cf
Gitlab.network <roque.souza@portodigital.pt>
2021-01-05 19:04:20 +0000

    metodos estaticos e tamplate string

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,7 @@
 class NegociacaoController {
     
     constructor() {
-        let $ = document.querySelector.bind(document);
+         let $ = document.querySelector.bind(document);
          this._inputData = $('#data');
          this._inputQuantidade = $('#quantidade');
          this._inputValor= $('#valor');
@@ -9,25 +9,14 @@ class NegociacaoController {
     
     adiciona () {
         event.preventDefault();
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)  
-        );
         
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
-        let diaMesAno = negociacao.data.getDate() 
-        + '/' + (negociacao.data.getMonth() + 1 ) 
-        + '/' + negociacao.data.getFullYear();
-        
-        console.log(diaMesAno);
-      
-
+        console.log(DateHelper.dataParaTexto(negociacao.data));
         // adicionar a negociaçao numa lista
     }
 }
