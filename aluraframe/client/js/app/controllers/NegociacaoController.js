cd87817a92490828cb38ff876378ebba9a9c5a20
Gitlab.network <roque.souza@portodigital.pt>
2021-01-05 11:22:03 +0000

    trabalhando com date

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,33 @@
 class NegociacaoController {
     
-
+    constructor() {
+        let $ = document.querySelector.bind(document);
+         this._inputData = $('#data');
+         this._inputQuantidade = $('#quantidade');
+         this._inputValor= $('#valor');
+    }
+    
     adiciona () {
         event.preventDefault();
-        
-        let inputData = document.querySelector('#data');
-        let inputQuantidade = document.querySelector('#quantidade');
-        let inputValor= document.querySelector('#valor');
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map(function(item, indice) {
+                return item - indice % 2
+            })
+            
+        );
+        console.log(data);
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+
+        // let negociacao = new Negociacao(
+        //     this._inputData.value.split('-'),
+        //     this._inputQuantidade.value,
+        //     this._inputValor.value
+        // );
+
+        // console.log(negociacao);
+
+        // adicionar a negociaçao numa lista
     }
 }
