f9fd995586edf0e1fe9e01d92265acf6b068b8f7
Gitlab.network <roque.souza@portodigital.pt>
2021-01-07 14:57:48 +0000

    arrow function  in view

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -18,21 +18,18 @@ class NegociacoesView {
 
             <tbody>
             ${model.negociacoes.map(n => `
-                  <tr>
-                      <td>${DateHelper.dataParaTexto(n.data)}</td>
-                      <td>${n.quantidade}</td>
-                      <td>${n.valor}</td>
-                      <td>${n.volume}</td>
-                  </tr>
-                `).join('')}
+                <tr>
+                    <td>${DateHelper.dataParaTexto(n.data)}</td>
+                    <td>${n.quantidade}</td>
+                    <td>${n.valor}</td>
+                    <td>${n.volume}</td>
+                </tr>
+            `).join('')}
             </tbody>
             <tfoot>
-            <td colspan="3"></td>
+                <td colspan="3"></td>
                 <td>
-                ${model.negociacoes.reduce(function(total, n) {
-                    return total + n.volume;
-                    }, 0.0)
-                }
+                    ${ model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
                 </td>
             </tfoot>
         </table>
