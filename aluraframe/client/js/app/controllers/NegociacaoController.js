ef8a246e14b84099be7fe0eae091541dfaba614a
Gitlab.network <roque.souza@portodigital.pt>
2021-01-11 13:01:45 +0000

    metodo proxy

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,13 +1,36 @@
 class NegociacaoController {
-    
+
     constructor() {
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
-        this._inputValor= $('#valor');
-        
-        this._listaNegociacoes = new ListaNegociacoes(model => 
-            this._negociacoesView.update(model));
+        this._inputValor = $('#valor');
+
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+
+            get(target, prop, receiver) {
+
+                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {
+
+                    return function () {
+
+                        console.log(`método '${prop}' interceptado`);
+
+                        Reflect.apply(target[prop], target, arguments);
+
+                        self._negociacoesView.update(target);
+
+                    }
+                }
+
+                return Reflect.get(target, prop, receiver);
+            }
+        });
+
+
+        // this._listaNegociacoes = new ListaNegociacoes(model => 
+        //     this._negociacoesView.update(model)); 
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -16,16 +39,16 @@ class NegociacaoController {
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
     }
-    
-    
-    adiciona () {
+
+
+    adiciona() {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        // this._negociacoesView.update(this._listaNegociacoes);
+        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociaçao adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
-        
+
         this._limpaFormulario()
 
     }
@@ -33,8 +56,8 @@ class NegociacaoController {
     apaga() {
 
         this._listaNegociacoes.esvazia();
-        // this._negociacoesView.update(this._listaNegociacoes);
-    
+        this._negociacoesView.update(this._listaNegociacoes);
+
         this._mensagem.texto = "Negociações removidas com sucesso";
         this._mensagemView.update(this._mensagem);
     }
--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,22 +1,18 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor() {
         this._negociacoes = [];
-        this._armadilha = armadilha;
-        // this._contexto = contexto;
-  }
+        
+    }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
-        // Reflect.apply(this._armadilha, this._contexto, [this]);
+        
     }
 
     esvazia() {
-
         this._negociacoes = [];
-        this._armadilha(this);
-        // Reflect.apply(this._armadilha, this._contexto, [this]);
+        
     }
 
 
