3dd4ef4e6f0536756bfb5a53743e03b086bcb268
Gitlab.network <roque.souza@portodigital.pt>
2021-01-14 11:54:05 +0000

    Update NegociacaoController.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -32,19 +32,31 @@ class NegociacaoController {
     }
 
     importaNegociacoes() {
-        let service = new NegociacaoService();
 
-        Promise.all([
-            service.obterNegociacoesDaSemana(),
-            service.obterNegociacoesDaSemanaAnterior(),
-            service.obterNegociacoesDaSemanaRetrasada()]
-        ).then(negociacoes => {
-            negociacoes
-                .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso';
-        })
-            .catch(erro => this._mensagem.texto = erro);
+        let service = new NegociacaoService();
+        service
+            .obterNegociacoes()
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = 'Negociações do período importadas'   
+            }))
+            .catch(erro => this._mensagem.texto = erro); 
+            
+        // let service = new NegociacaoService();
+
+        // Promise.all([
+        //     service.obterNegociacoesDaSemana(),
+        //     service.obterNegociacoesDaSemanaAnterior(),
+        //     service.obterNegociacoesDaSemanaRetrasada()]
+        // ).then(negociacoes => {
+        //     negociacoes
+        //         .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+        //         .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+        //     this._mensagem.texto = 'Negociações importadas com sucesso';
+        // })
+        //     .catch(erro => this._mensagem.texto = erro);
+
+
     }
 
     _criaNegociacao() {
